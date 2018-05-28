4306ccbde890d4e2427d2798b037888ccb88653b
Ahmed Gamal El-Taweel <ahmedtaweel96@gmail.com>
2018-05-29 00:22:47 +0200

    Add files via upload

---
+++ Weather Chatbot/config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    FB_PAGE_TOKEN: 'EAACTwBpSPm0BAKgHTsCjzFHrkQfpctrTmhz40HyDlHH9tcZCMY316YeaojLcdRvoFb60aZBoz8xRJcXLGArBePempW98hDQruzCcOij78oSu6vTlubw6DEKcXTAZBVXenn2WzZCZC0DIG1VaZBbhV8oEdpm4O700s8x7X6ZCjdeDAZDZD',
+    FB_VERIFY_TOKEN: 'my_app_123',
+    API_AI_CLIENT_ACCESS_TOKEN: '0d4c5d617260410cacfe7f1e37a54062',
+    FB_APP_SECRET: '88f83cc9724bb5f339d5eea378c32b63',
+    SERVER_URL: "https://testappagt.herokuapp.com/",
+    WEATHER_API_KEY:'7b8491b5ee05e5c5dfceb6d1bdc32003'
+};
